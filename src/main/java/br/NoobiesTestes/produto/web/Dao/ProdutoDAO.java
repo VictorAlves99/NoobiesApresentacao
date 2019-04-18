2cdf2343b74ca1b8edf8e02d5bb20fedbade5cbe
NATHAN OLIVEIRA SANTOS <nathan.osantos@senacsp.edu.br>
2019-04-18 19:32:33 -0300

    DAO 2.0

--- src/main/java/br/NoobiesTestes/produto/web/Dao/ProdutoDAO.java
+++ src/main/java/br/NoobiesTestes/produto/web/Dao/ProdutoDAO.java
@@ -69,13 +69,164 @@ public class ProdutoDAO {
         }
     }
 }
-    public void excluirProduto(){
+    //incompleto
+    public void excluirProduto(int id){
+        {
+        PreparedStatement stmt = null;
+        Connection conn = null;
+        //Preparar string sql
+        String sql = "DELETE FROM DB_LOJA_NOOBIES.TB_PRODUTOS WHERE ID ="+id+"";//INSERIR VARIÁVEL COM ID
         
+        //Obten conexão para SQL workbench
+        try 
+        {
+            conn = obterConexao();
+            stmt = conn.prepareStatement(sql);                  
+            stmt.executeUpdate();
+        } 
+        catch (SQLException ex) 
+        {
+            System.out.println("Não foi possível executar. SQL Exception");
+        } 
+        catch (ClassNotFoundException ex) 
+        {
+            System.out.println("Não foi possível executar. ClassNotFound EXception");
+        } finally 
+        {
+            if (stmt != null) 
+            {
+                try 
+                {
+                    stmt.close();
+                } 
+                catch (SQLException ex) 
+                {
+                    System.out.println("Erro ao fechar stmt.");
+                }
+            }
+            if (conn != null) 
+            {
+                try 
+                {
+                    conn.close();
+                } 
+                catch (SQLException ex) 
+                {
+                    System.out.println("Erro ao fechar conn.");
+                }
+            }
+        }
     }
-    public void editarProduto(){
-        
     }
-    public void consultarProduto(){
+    public void editarProduto(Produto produto){
+        //Abrir conexao e deixa ela null
+        PreparedStatement stmt = null;
+        Connection conn = null;
         
+        //Preparar string sql
+        String sql = "UPDATE DB_LOJA_NOOBIES.TB_PRODUTOS SET NOME_PRODUTO = ?, DESCRICAO = ?, VALOR_COMPRA = ?, VALOR_VENDA = ?, QUANTIDADE = ?"
+                + "WHERE COD_PRODUTO = ";//INSERIR VARIÁVEL COM ID
+        
+        //Obten conexão para SQL workbench
+        try 
+        {
+            conn = obterConexao();
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, produto.getNomeProd());
+            stmt.setString(2, produto.getDescProd());
+            stmt.setDouble(3, produto.getValorCompra());
+            stmt.setDouble(4, produto.getValorVenda());
+            stmt.setInt(5, produto.getQuantidade());           
+
+            // 2) Executar SQL
+            stmt.executeUpdate();
+        } 
+        catch (SQLException ex) 
+        {
+            System.out.println("Não foi possível executar. SQL Exception");
+        } 
+        catch (ClassNotFoundException ex) 
+        {
+            System.out.println("Não foi possível executar. ClassNotFound EXception");
+        } finally 
+        {
+            if (stmt != null) 
+            {
+                try 
+                {
+                    stmt.close();
+                } 
+                catch (SQLException ex) 
+                {
+                    System.out.println("Erro ao fechar stmt.");
+                }
+            }
+            if (conn != null) 
+            {
+                try 
+                {
+                    conn.close();
+                } 
+                catch (SQLException ex) 
+                {
+                    System.out.println("Erro ao fechar conn.");
+                }
+            }
+        }
+    }
+    public void consultarProduto(Produto produto){
+                //Abrir conexao e deixa ela null
+        PreparedStatement stmt = null;
+        Connection conn = null;
+        
+        //Preparar string sql
+        String sql = "SELECT ";//INSERIR VARIÁVEL COM ID
+        
+        //Obten conexão para SQL workbench
+        try 
+        {
+            conn = obterConexao();
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, produto.getNomeProd());
+            stmt.setString(2, produto.getDescProd());
+            stmt.setDouble(3, produto.getValorCompra());
+            stmt.setDouble(4, produto.getValorVenda());
+            stmt.setInt(5, produto.getQuantidade());           
+
+            // 2) Executar SQL
+            stmt.executeUpdate();
+        } 
+        catch (SQLException ex) 
+        {
+            System.out.println("Não foi possível executar. SQL Exception");
+        } 
+        catch (ClassNotFoundException ex) 
+        {
+            System.out.println("Não foi possível executar. ClassNotFound EXception");
+        } finally 
+        {
+            if (stmt != null) 
+            {
+                try 
+                {
+                    stmt.close();
+                } 
+                catch (SQLException ex) 
+                {
+                    System.out.println("Erro ao fechar stmt.");
+                }
+            }
+            if (conn != null) 
+            {
+                try 
+                {
+                    conn.close();
+                } 
+                catch (SQLException ex) 
+                {
+                    System.out.println("Erro ao fechar conn.");
+                }
+            }
+        }
     }
 }
